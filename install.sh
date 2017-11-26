4ab0d09e1f073189bdb93265fab0704ce45d226c
Gideon Spierings <gspierings@vaa.com>
2017-11-26 22:02:57 +0100

    Removed some use echos because theyre pretty much invisable anyway

--- install.sh
+++ install.sh
@@ -6,15 +6,8 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Update the OS and Install Xcode Tools
-echo "------------------------------"
-echo "Updating OSX.  If this requires a restart, run the script again."
-
 # Install all available updates
 sudo softwareupdate -iva
-
-echo "------------------------------"
-echo "Installing Xcode Command Line Tools."
 
 # Install Xcode command line tools
 xcode-select --install
