533abf56fb0251a7068d4ebe2d43ec43225d55e4
Gideon Spierings <gspierings@vaa.com>
2017-11-26 15:52:41 +0100

    Updated brew.sh with a whole bunch of stuff

--- brew.sh
+++ brew.sh
@@ -1,47 +1,105 @@
-# Install Homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Check for Homebrew, install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
-read -p "Press enter to continue"
+# Make sure we’re using the latest Homebrew.
+brew update
 
-# Install Homebrew Cask
-brew tap caskroom/cask
-brew install brew-cask
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
+# Install GNU core utilities (those that come with OS X are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install more recent versions of some OS X tools.
+brew install vim --override-system-vi
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install homebrew/dupes/screen
+brew install homebrew/php/php55 --with-gmp
+
+# Install some CTF tools; see https://github.com/ctfs/write-ups.
+brew install aircrack-ng
+brew install bfg
+brew install binutils
+brew install binwalk
+brew install cifer
+brew install dex2jar
+brew install dns2tcp
+brew install fcrackzip
+brew install foremost
+brew install hashpump
+brew install hydra
+brew install john
+brew install knock
+brew install netpbm
+brew install nmap
+brew install pngcheck
+brew install socat
+brew install sqlmap
+brew install tcpflow
+brew install tcpreplay
+brew install tcptrace
+brew install ucspi-tcp # `tcpserver` etc.
+brew install homebrew/x11/xpdf
+brew install xz
+
+# Install other useful binaries.
+brew install ack
+brew install dark-mode
+
+#brew install exiv2
+brew install git
+brew install git-extras
+brew install imagemagick --with-webp
+brew install lua
+brew install ssh-copy-id
+brew install tree
+brew install webkit2png
+brew install pkg-config libffi
+brew install pandoc
+
+# Install Cask
+brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 
-read -p "Press enter to continue"
+# Core casks
+brew cask install --appdir="/Applications" alfred
+brew cask install --appdir="~/Applications" iterm2
+brew cask install --appdir="~/Applications" java
+brew cask install --appdir="~/Applications" xquartz
 
-# Update Homebrew
-brew update
-brew upgrade
+# Development tool casks
+brew cask install --appdir="/Applications" visual-studio-code
+brew cask install --appdir="/Applications" virtualbox
 
-read -p "Press enter to continue"
+# Misc casks
+brew cask install --appdir="/Applications" google-chrome
+brew cask install --appdir="/Applications" firefox
+brew cask install --appdir="/Applications" slack
+brew cask install --appdir="/Applications" dropbox
+brew cask install --appdir="/Applications" 1password
+brew cask install --appdir="/Applications" gimp
 
-# Install packages
-brew_apps=(
-  docker
-  git
-  magic-wormhole
-  node
-  tree
-  vim
-  wget
-)
+# Install Docker, which requires virtualbox
+brew install docker
+brew install boot2docker
 
-brew install "${brew_apps[@]}"
+# Install developer friendly quick look plugins; see https://github.com/sindresorhus/quick-look-plugins
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
 
-read -p "Press enter to continue"
-
-# Instal cask packages
-cask_apps=(
-  alfred
-  dropbox
-  firefox
-  google-chrome
-  spotify
-  virtualbox
-  visual-studio-code
-)
-
-brew install "${cask_apps[@]}"
-
-read -p "Press enter to continue"
+# Remove outdated versions from the cellar.
+brew cleanup
