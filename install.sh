e2613eb89bf34b2a4a8e55defe4ba1e0c95496d6
Gideon Spierings <gideon.spierings@devolksbank.nl>
2018-09-24 16:24:19 +0200

    xcode.sh is now actually being used

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 sudo softwareupdate -iva
 
 # Install Xcode command line tools
-xcode-select --install
+source ./xcode.sh
 
 # Install Hombrew and apps
 source ./brew.sh
