00275babdeba2ea7ec58f0be971a0d5abe7bd175
Gideon Spierings <gideongideon@gmail.com>
2018-10-12 17:05:00 +0200

    Changed the Vim install command because the option --override-system-vi is deprecated

--- brew.sh
+++ brew.sh
@@ -27,7 +27,7 @@ brew install gnu-sed --with-default-names
 brew install wget --with-iri
 
 # Install more recent versions of some OS X tools.
-brew install vim --override-system-vi
+brew install vim --with-override-system-vi
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
