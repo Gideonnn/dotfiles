786925a2c7db3649b66978f960f5068537087d29
Gideon Spierings <gideongideon@gmail.com>
2018-10-12 15:32:11 +0200

    Homebrew no longer auto installs in brew.sh, it now just shows the user a message

--- brew.sh
+++ brew.sh
@@ -2,8 +2,8 @@
 
 # Check for Homebrew, install if we don't have it
 if test ! $(which brew); then
-  echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null
+  echo "Homebrew is not installed. Please install it first"
+  return;
 fi
 
 # Make sure we’re using the latest Homebrew.
