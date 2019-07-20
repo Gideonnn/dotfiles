08254c15a3c6e36bf551e9c04c3130a11e9c3db0
Gideon Spierings <gideongideon@gmail.com>
2019-07-20 16:38:02 +0200

    Remove a broken softlink and a useless comment

--- brew.sh
+++ brew.sh
@@ -13,9 +13,7 @@ brew update
 brew upgrade
 
 # Install GNU core utilities (those that come with OS X are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
-sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
