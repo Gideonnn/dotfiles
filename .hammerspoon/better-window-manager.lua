4edc05b34a08c49cc93ab271ba1530d17670bd3e
Gideon Spierings <gideongideon@gmail.com>
2023-12-24 20:37:10 +0100

    Save hammerspoon dir

--- .hammerspoon/better-window-manager.lua
+++ .hammerspoon/better-window-manager.lua
@@ -1,10 +1,4 @@
--- Resize a window to a specified size
-function resizeWindow(win, size)
-  local f = win:frame()
-  f.w = size.w
-  f.h = size.h
-  win:setFrame(f)
-end
+
 
 -- Center a window on a specified point, taking into account the macOS menu bar
 function centerOnPoint(win, x, y)
@@ -50,8 +44,47 @@ function resizeHorizontally(step)
   end
 end
 
+function drawPoint(x, y)
+  local point = hs.canvas.new{x=x, y=y, w=100, h=100}
+  point[1] = {
+    type = "circle",
+    action = "fill",
+    fillColor = {["red"]=1,["blue"]=0,["green"]=0,["alpha"]=1},
+    radius = 50
+  }
+  point:show()
+end
+
+function showAnchor(x, y)
+  local circleSize = 30
+  local textSize = 18
+  local canvasRect = {x = x - circleSize/2, y = y - circleSize/2, w = circleSize, h = circleSize}
+  local canvas = hs.canvas.new(canvasRect)
+  canvas:appendElements({
+    type = "circle",
+    action = "fill",
+    fillColor = {red = 1},
+  })
+  canvas:appendElements({
+    type = "text",
+    text = "1",
+    textColor = {white = 1},
+    textSize = textSize,
+    textAlignment = "center",
+    frame = {x = 0, y = 4, w = circleSize, h = textSize}
+  })
+  canvas:show()
+  hs.timer.doAfter(2, function() canvas:delete(.5) end)
+end
+
+
+local windowFrame = hs.window.desktop():frame()
+showAnchor(windowFrame.w/2, windowFrame.h/2)
+
+
 -- Bind hotkeys for resizing windows
-hs.hotkey.bind({ "alt","shift", "ctrl", "cmd" }, "up", resizeVertically(50))
-hs.hotkey.bind({ "alt","shift", "ctrl", "cmd" }, "down", resizeVertically(-50))
+hs.hotkey.bind({ "alt","shift", "ctrl", "cmd" }, "h", resizeVertically(50))
+hs.hotkey.bind({ "alt","shift", "ctrl", "cmd" }, "t", resizeVertically(-50))
 hs.hotkey.bind({ "alt","shift", "ctrl", "cmd" }, "left", resizeHorizontally(-75))
 hs.hotkey.bind({ "alt","shift", "ctrl", "cmd" }, "right", resizeHorizontally(75))
+hs.hotkey.bind({ "alt","shift", "ctrl", "cmd" }, "", resizeHorizontally(75))
